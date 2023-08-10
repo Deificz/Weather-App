46e162a581af740b3e48c931062cdb1d956c7777
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-10 20:26:56 +0800

    Changed http links to https

--- src/Model.js
+++ src/Model.js
@@ -2,7 +2,7 @@ export const Model = (() => {
     
     const fetchData = async (city) =>{
         try{
-            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=26fce71067d84e15bf3132813230908&q=${city}`, {mode: 'cors'});
+            const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=26fce71067d84e15bf3132813230908&q=${city}`, {mode: 'cors'});
             const data = await response.json();
             return data;
         }catch{
